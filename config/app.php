78edec0decd2109b0528408fa0f06a5e7fb64294
tlb <tenglibai@163.com>
2017-11-02 12:19:50 +0800

    Require:laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         /*
          * Application Service Providers...
          */
